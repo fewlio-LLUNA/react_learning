032687dc5e02d2ab11642de71bc66af6a05b9412
fewlio-LLUNA <seraja0824ryoiki@gmail.com>
2024-05-31 16:18:11 +0900

    react_learningを作成しました

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+function DisplayName(props){
+  console.log(props)
+  return(
+    <h1 className='DisplayName'>こんにちは {props.name}さん :{props.age}歳 {props.grade}年</h1>
+  )
+}
+
 function App() {
+
+  const[cnt, setCnt]=useState(0) //値を保持する
+  let letCnt = 0
+  console.log(cnt,setCnt)
+
+  const students = [{name:'tanaka', age:'20', grade:'3'},
+                    {name:'satou', age:'21', grade:'3'},
+                    {name:'hogehoge', age:'22', grade:'4'}]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {students.map(function(student, index){
+        return <DisplayName key={index} name={student.name} age={student.age} grade={student.grade}/>
+      })}
+      {/* <DisplayName name="tanaka" age="20"/>
+      <DisplayName name="satou"/>
+      <DisplayName grade="3"/> */}
+      <p>hogehoge</p>
+
+      <div>{cnt} {letCnt}</div>
+
+      <button onClick={function(){
+        console.log("+")
+        setCnt(cnt+1)
+        letCnt++
+      }}>+</button>
+
+      <button onClick={function(){
+        console.log("-")
+        setCnt(cnt-1)
+        letCnt--
+      }}>-</button>
+
     </div>
   );
 }
